dc623690cd614e9356a72b33326d1f00bf69c102
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2024-09-30 00:41:43 -0500

    xd
    xd

--- tarea4/prueba/programa2.c
+++ tarea4/prueba/programa2.c
@@ -29,41 +29,22 @@ void *thread_function(void *arg) {
 
         printf("Thread %ld recibió: Señal %d para PID %d\n", mtype, message.signal, message.pid);
 
+        // Manejar el caso especial de SIGSTOP (19)
+        if (message.signal == 19) {
+            printf("PID %d fue afectado por la señal 19 (SIGSTOP)\n", message.pid);
+        }
+
         // Enviar la señal al proceso
         if (kill(message.pid, message.signal) < 0) {
             perror("kill");
         } else {
-            printf("Señal %d enviada al proceso %d\n", message.signal, message.pid);
+            if (message.signal != 19) {
+                printf("Señal %d enviada al proceso %d\n", message.signal, message.pid);
+            }
         }
     }
 
     return NULL;
 }
 
-int main() {
-    int msqid;
-    key_t key = 1234;
-    pthread_t thread1, thread2;
-
-    // Obtener la cola de mensajes
-    if ((msqid = msgget(key, 0666)) < 0) {
-        perror("msgget");
-        exit(1);
-    }
-
-    // Crear los threads
-    if (pthread_create(&thread1, NULL, thread_function, &msqid) != 0) {
-        perror("pthread_create");
-        exit(1);
-    }
-    if (pthread_create(&thread2, NULL, thread_function, &msqid) != 0) {
-        perror("pthread_create");
-        exit(1);
-    }
-
-    // Esperar a que los threads terminen (lo cual nunca sucederá en este caso)
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-
-    return 0;
-}
+// ... (el resto del código permanece igual)
